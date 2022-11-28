1f92a1ede6456cf5c897cb961020c9ec62e00cc1
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-11-27 23:18:08 -0500

    Created get_word() and check(). Updated lines 24, 38, 39, draw_hangman(), and main()

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,27 @@
 import pygame
 from sys import exit
+from random import randint
 
 
-def game():
+def get_word(filename):
+    '''
+    This function gets and returns a random word from a text file that contains a lot of words.
+    :param filename: String
+    :return word_file_lines: String
+    '''
+    try: # checking to see if you have a file.
+        with open(filename, "r") as word_file:
+            word_file_lines = word_file.readlines()
+            return word_file_lines[randint(0, len(word_file_lines) - 1)].strip()
+    except FileNotFoundError:
+        # PLACEHOLDER, PRINT AN ERROR MESSAGE ON THE GUI
+        pass
+
+
+def game(file):
     '''
     This is the hangman GUI.
+    :param: file: String
     :return:
     '''
 
@@ -18,6 +35,9 @@ def game():
     input_box = pygame.Rect(150, 320, 100, 40)
     user_input = ""
 
+    # game
+    guesses = 6
+
     while True:
 
         for event in pygame.event.get():
@@ -29,21 +49,35 @@ def game():
                 if event.key == pygame.K_BACKSPACE:
                     user_input = user_input[0:-1]
                 elif event.key == pygame.K_RETURN:
-                    pass
+                    check(user_input)
                 else:
                     user_input += event.unicode
 
         window.fill((254, 254, 254)) # window background color (whitish)
 
-        draw_hangman(window, 0)
+        draw_hangman(window, guesses)
 
         # User input box
-        pygame.draw.rect(window, ("Blue"), input_box, 2)
+        pygame.draw.rect(window, "blue", input_box, 2)
         text_surface = base_font.render(user_input, True, (0, 0, 0))
         window.blit(text_surface, (input_box.x + 5, input_box.y + 5))
 
         pygame.display.update()
         clock.tick(60)
+
+
+def check(user_input):
+    if len(user_input) > 1:
+        if user_input == "end":
+            # End Game
+            pass
+        elif user_input == "restart":
+            # Restart game
+            pass
+        else:
+            pass  # Error message
+    else:
+        pass
 
 
 def draw_hangman(window, chances):
@@ -53,92 +87,94 @@ def draw_hangman(window, chances):
     :param chances: int
     :return:
     '''
-    BLACK = (0, 0, 0)
+
+    black = (0, 0, 0)
 
     if chances == 6:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
     elif chances == 5:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
 
     elif chances == 4:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
-        pygame.draw.line(window, BLACK, (250, 190), (250, 240), 2)  # Body
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
+        pygame.draw.line(window, black, (250, 190), (250, 240), 2)  # Body
 
     elif chances == 3:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
-        pygame.draw.line(window, BLACK, (250, 190), (250, 240), 2)  # Body
-        pygame.draw.line(window, BLACK, (250, 195), (240, 240), 2)  # Left Arm
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
+        pygame.draw.line(window, black, (250, 190), (250, 240), 2)  # Body
+        pygame.draw.line(window, black, (250, 195), (240, 240), 2)  # Left Arm
 
     elif chances == 2:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
-        pygame.draw.line(window, BLACK, (250, 190), (250, 240), 2)  # Body
-        pygame.draw.line(window, BLACK, (250, 195), (240, 240), 2)  # Left Arm
-        pygame.draw.line(window, BLACK, (250, 195), (260, 240), 2)  # Right Arm
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
+        pygame.draw.line(window, black, (250, 190), (250, 240), 2)  # Body
+        pygame.draw.line(window, black, (250, 195), (240, 240), 2)  # Left Arm
+        pygame.draw.line(window, black, (250, 195), (260, 240), 2)  # Right Arm
 
     elif chances == 1:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
-        pygame.draw.line(window, BLACK, (250, 190), (250, 240), 2)  # Body
-        pygame.draw.line(window, BLACK, (250, 195), (240, 240), 2)  # Left Arm
-        pygame.draw.line(window, BLACK, (250, 195), (260, 240), 2)  # Right Arm
-        pygame.draw.line(window, BLACK, (250, 240), (240, 280), 2)  # Left Leg
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
+        pygame.draw.line(window, black, (250, 190), (250, 240), 2)  # Body
+        pygame.draw.line(window, black, (250, 195), (240, 240), 2)  # Left Arm
+        pygame.draw.line(window, black, (250, 195), (260, 240), 2)  # Right Arm
+        pygame.draw.line(window, black, (250, 240), (240, 280), 2)  # Left Leg
 
     else:
         # Gallows
-        pygame.draw.line(window, BLACK, (150, 100), (250, 100), 3)  # Top
-        pygame.draw.line(window, BLACK, (250, 100), (250, 150), 3)  # Rope
-        pygame.draw.line(window, BLACK, (150, 100), (150, 310), 3)  # Trunk
-        pygame.draw.line(window, BLACK, (150, 310), (200, 310), 3)  # Base
+        pygame.draw.line(window, black, (150, 100), (250, 100), 3)  # Top
+        pygame.draw.line(window, black, (250, 100), (250, 150), 3)  # Rope
+        pygame.draw.line(window, black, (150, 100), (150, 310), 3)  # Trunk
+        pygame.draw.line(window, black, (150, 310), (200, 310), 3)  # Base
 
         # Man
-        pygame.draw.circle(window, BLACK, (250, 170), 20, 2)  # Head
-        pygame.draw.line(window, BLACK, (250, 190), (250, 240), 2)  # Body
-        pygame.draw.line(window, BLACK, (250, 195), (240, 240), 2)  # Left Arm
-        pygame.draw.line(window, BLACK, (250, 195), (260, 240), 2)  # Right Arm
-        pygame.draw.line(window, BLACK, (250, 240), (240, 280), 2)  # Left Leg
-        pygame.draw.line(window, BLACK, (250, 240), (260, 280), 2)  # Right Leg
+        pygame.draw.circle(window, black, (250, 170), 20, 2)  # Head
+        pygame.draw.line(window, black, (250, 190), (250, 240), 2)  # Body
+        pygame.draw.line(window, black, (250, 195), (240, 240), 2)  # Left Arm
+        pygame.draw.line(window, black, (250, 195), (260, 240), 2)  # Right Arm
+        pygame.draw.line(window, black, (250, 240), (240, 280), 2)  # Left Leg
+        pygame.draw.line(window, black, (250, 240), (260, 280), 2)  # Right Leg
 
 
 def main():
-    game()
+    word = get_word("word_bank.txt")
+    game(word)
 
 
 if __name__ == '__main__':
